45c6849647ed4e50d1dd552128e596adc0a9fc9e
Amaan Khan <amaan.mca23.du@gmail.com>
2025-05-06 18:49:37 +0530

    added comment

--- src/main/java/net/rest/journalApp/JournalEntryController/JournalApplication.java
+++ src/main/java/net/rest/journalApp/JournalEntryController/JournalApplication.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 public class JournalApplication {
+//	comment added
 	public static void main(String[] args) {
 		SpringApplication.run(JournalApplication.class, args);
 	}
